bddf227b14f10abfe618c42edfb04b8c9a0b5a32
oraxswitcher <raouf200026@gmail.com>
2020-10-18 22:38:02 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Alata&family=Signika+Negative:wght@300&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Sigmar+One&display=swap" rel="stylesheet"> 
+    <link href="https://fonts.googleapis.com/css2?family=Simonetta&display=swap" rel="stylesheet"> 
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css2?family=Amarante&display=swap" rel="stylesheet"> 
+    <link href="https://fonts.googleapis.com/css2?family=Balsamiq+Sans:wght@700&display=swap" rel="stylesheet">     <link rel="stylesheet" href="./css/style1.css">
+    <title>Pets4U</title>
+</head>
+<body>
+    <header>
+        <div class="navbar">
+            <img src="./imgs/withLogo.png" alt="logo">
+            <h1><span>Pets</span><span>4</span><span>U</span></h1>
+            <div class="nav">
+                <a href="#">About-us</a>
+                <a href="#">Our-pets</a>
+                <a href="#">Contact-us</a>
+                <a href="#">Locations</a>
+            </div>
+        </div>
+        <div class="content-header">
+            <div class="text">
+                <h2><span>Welcome to</span> <span>Pets</span><span>4</span><span>U</span></h2>
+                <p>The right place</br> for getting your own pet</p>
+                <button><span>Discover More</span></button>
+            </div>
+            <div class="img"></div>
+        </div>
+        <div class="paws"></div>
+    </header>
+    <p class="line">______________________________________________________</p>
+    <section class="sec1">
+        <div class="bio">
+            <div class="text-box">
+                <h2>Our Goal</h2>
+                <p>we don’t want any animal to be stray by his own , animals deserve love as we do 
+                    so we save any animal we found on the road and you can be a part of that :) </p>
+            </div>
+            <div class="cat"></div>
+        </div>
+        <div class="paws2"></div>
+    </section>
+    <p class="line">______________________________________________________</p>
+    <section class="sec2">
+        <h2>Our Pets</h2>
+        <div class="cards-line">
+            <div class="cards">
+                <img src="./imgs/adobt1.jpg" alt="Sousou">
+                <div class="infos">
+                    <h3>Sousou</h3>
+                    <p>male , adult</p>
+                    <p>Los angeles,CA</p>
+                </div>
+            </div>
+            <div class="cards">
+                <img src="./imgs/adobt2.jpg" alt="Ertha">
+                <div class="infos">
+                    <h3>Ertha</h3>
+                    <p>Female , young</p>
+                    <p>Beverly Hills,CA</p>
+                </div>
+            </div>
+            <div class="cards">
+                <img src="./imgs/adobt3.jpg" alt="ceciro">
+                <div class="infos">
+                    <h3>Ceciro</h3>
+                    <p>male , young</p>
+                    <p>Beverly Hills,CA</p>
+                </div>
+            </div>
+            <div class="cards">
+                <h3>See more pets</h3>
+                <img src="./imgs/discoverpets.jpg" alt="pets">
+            </div>
+        </div>
+        <div class="paws3"></div>
+    </section>
+    <p class="line">______________________________________________________</p>
+
+    <footer>
+        <div class="support">
+            <h3>YOUR SUPPORT</h3>
+            <p>By your donation ,  more pets can be saved
+                more workers to help , more food to provide,
+                and a full suport for us </p>
+                <button>DONATE</button>
+        </div>
+        <img src="./imgs/footer.png" alt="dog">
+        <div class="contact">
+            <h3>Contact-Us</h3>
+            <div class="social">
+                <a href="#"><i class="fab fa-facebook-f"></i> Facebook</a>
+                <a href="#"><i class="fab fa-instagram"></i> Instagram</a>
+                <a href="#"><i class="fab fa-twitter"></i> Twitter</a>
+                <a href="#"><i class="fab fa-whatsapp"></i> Whatsapp</a>
+            </div>
+        </div>
+        <div class="informations">
+            <h3>Informations</h3>
+            <div class="informs">
+                <a href="#"><i class="fas fa-map-marker-alt"></i>Los angeles,CA</a>
+                <a href="#"><i class="fas fa-phone-alt"></i>(555) 555-555</a>
+                <a href="#"><i class="far fa-envelope"></i> Pets4U@gmail.com</a>
+            </div>
+        </div>
+        <div class="paws4"></div>
+    </footer>
+    <script src="https://kit.fontawesome.com/fdddec0283.js" crossorigin="anonymous"></script>
+</body>
+</html>
